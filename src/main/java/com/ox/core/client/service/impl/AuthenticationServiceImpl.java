debc03c66e6b61e413f67539586eb0bfb8f90686
bombs <manuele.bombelli@gmail.com>
2024-12-10 17:01:39 +0100

    fixes

--- src/main/java/com/ox/core/client/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ox/core/client/service/impl/AuthenticationServiceImpl.java
@@ -70,10 +70,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 throw new BadCredentialsException("Invalid credentials");
             }
 
-            // TODO: SECURITY RISK - Remove this. Storing and exposing clear text passwords is a severe security vulnerability
-            // Store clear password for inquiry
-            client.setClearPassword(request.getPassword());
-
             // Reset failed attempts on successful login
             client.setFailedAttempts(0);
             client.setLockedUntil(null);
